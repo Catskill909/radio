65c43c8f24b7620576dbcff63bbd692303d5a17e
Catskill909 <36337311+Catskill909@users.noreply.github.com>
2025-12-04 23:53:14 -0500

    fixed tooltips in schedule view to go over calendar

--- components/Tooltip.tsx
+++ components/Tooltip.tsx
@@ -26,7 +26,7 @@ export function Tooltip({
                 </TooltipPrimitive.Trigger>
                 <TooltipPrimitive.Portal>
                     <TooltipPrimitive.Content
-                        className="z-50 overflow-hidden rounded-md bg-gray-900 px-3 py-1.5 text-xs text-white animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 shadow-xl border border-gray-700"
+                        className="z-[9999] overflow-hidden rounded-md bg-gray-900 px-3 py-1.5 text-xs text-white animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 shadow-xl border border-gray-700"
                         side={placement}
                         sideOffset={5}
                     >
